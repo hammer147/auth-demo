e0c08c479c4603d4c059f9e1bdf82d13f2412aee
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2021-08-01 12:04:21 +0200

    added Credentials Auth Provider & user login logic

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,30 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+import { verifyPassword } from '../../../lib/auth'
+import { connectToDatabase } from '../../../lib/db'
+
+export default NextAuth({
+  session: {
+    jwt: true
+  },
+  providers: [
+    Providers.Credentials({
+      async authorize(credentials: Record<'email' | 'password', string>) {
+        const client = await connectToDatabase()
+        const usersCollection = client.db().collection('users')
+        const user = await usersCollection.findOne({email: credentials.email})
+        if (!user) {
+          client.close()
+          throw new Error('User not found.')
+        }
+        const isValid = await verifyPassword(credentials.password, user.password)
+        if (!isValid) {
+          client.close()
+          throw new Error('Could not log in.')
+        }
+        client.close()
+        return { email: user.email} // will be used to generate JWT
+      }
+    })
+  ]
+})
