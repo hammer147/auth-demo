b443ee8a77de3dd2e72e07643951e1e18a71ff05
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2021-08-09 15:35:29 +0200

    switched to server side page guard

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,27 +1,46 @@
+import { GetServerSideProps } from 'next'
 import { getSession } from 'next-auth/client'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+// import { useRouter } from 'next/router'
+// import { useEffect, useState } from 'react'
 import AuthForm from '../components/auth/auth-form'
 
 function AuthPage() {
-  const [isLoading, setIsLoading] = useState(true)
-  const router = useRouter()
+  // const [isLoading, setIsLoading] = useState(true)
+  // const router = useRouter()
 
-  useEffect(() => {
-    getSession().then(session => {
-      if (session) {
-        router.replace('/')
-      } else {
-        setIsLoading(false)
-      }
-    })
-  }, [router])
+  // useEffect(() => {
+  //   getSession().then(session => {
+  //     if (session) {
+  //       router.replace('/')
+  //     } else {
+  //       setIsLoading(false)
+  //     }
+  //   })
+  // }, [router])
 
-  if (isLoading) {
-    return <p>Loading...</p>
-  }
-  
+  // if (isLoading) {
+  //   return <p>Loading...</p>
+  // }
+
   return <AuthForm />
+}
+
+export const getServerSideProps: GetServerSideProps = async context => {
+  const session = await getSession({ req: context.req })
+
+  if (session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: { session } // used by Provider in _app.tsx
+  }
+
 }
 
 export default AuthPage
