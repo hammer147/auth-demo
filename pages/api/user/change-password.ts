1c21e526691877bb59059179aab3ed48984474cf
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2021-08-14 13:51:50 +0200

    added api route to change password

---
+++ pages/api/user/change-password.ts
@@ -0,0 +1,59 @@
+import { MongoClient } from 'mongodb'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/client'
+import { hashPassword, verifyPassword } from '../../../lib/auth'
+import { connectToDatabase } from '../../../lib/db'
+
+type Data = {
+  message: string
+}
+
+type Passwords = {
+  oldPassword: string
+  newPassword: string
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  if (req.method !== 'PATCH') {
+    return
+  }
+
+  const session = await getSession({ req })
+  if (!session) {
+    return res.status(401).json({ message: 'Not authenticated' })
+  }
+
+  const { email } = session.user!
+  const { oldPassword, newPassword } = req.body as Passwords
+
+  let client: MongoClient
+
+  try {
+    client = await connectToDatabase()
+  } catch (error) {
+    return res.status(500).json({ message: 'Could not connect to database.' })
+  }
+
+  const usersCollection = client.db().collection('users')
+
+  const user = await usersCollection.findOne({ email })
+
+  if (!user) {
+    client.close()
+    return res.status(404).json({ message: 'User not found' })
+  }
+
+  const currentPassword = user.password
+  const passwordsAreEqual = await verifyPassword(oldPassword, currentPassword)
+
+  if (!passwordsAreEqual) {
+    client.close()
+    return res.status(403).json({ message: 'Invalid Password' })
+  }
+
+  const hashedPassword = await hashPassword(newPassword)
+  const result = usersCollection.updateOne({ email }, { $set: { password: hashedPassword } })
+
+  client.close()
+  res.status(200).json({ message: 'Password updated' })
+}
